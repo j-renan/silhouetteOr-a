f542fdaa7b5e465452a56415a5f47131f39fcfad
Moises <moises@rcbdesenvolvimento.com.br>
2018-07-02 20:54:43 -0300

    ajustou identacao

--- views/cliente.php
+++ views/cliente.php
@@ -11,25 +11,25 @@
 <body>
     <!--barra de navegação-->
     <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Silhouette Orça</a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Clientes <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Produtos</a></li>
-        <li><a href="#">Orçamentos</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Silhouette Orça</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Clientes <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Orçamentos</a></li>
+          </ul>
+        </div>
+      </div>
+  </nav>
 
 
 
