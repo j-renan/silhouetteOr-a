97b054359771e3aa865bc71decf19151516f6a79
Renan <desen.renan@gmail.com>
2018-07-02 20:40:45 -0300

    colocou estrutura para usar o bootstrap

---
+++ views/cliente.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Silhouette Orça Clientes</title>
+    <!--incluindo arquivo css-->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+    <!--incluindo arquivos necessarios para a biblioteca-->
+    <script src="../assets/js/jquery.min.js"></script>    
+    <script src="../assets/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
