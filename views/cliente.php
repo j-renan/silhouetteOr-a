8cb968f4407084049ea4a336618c7a0474d463ca
Renan <desen.renan@gmail.com>
2018-07-02 21:41:07 -0300

    criou os botoes

--- views/cliente.php
+++ views/cliente.php
@@ -32,13 +32,16 @@
   </nav>
 
   <div class="container">
+  <h1>Cadastro de Clientes</h1>
+  <hr/>
+  <br/>
   <form action="">
   <!--linha um id, nome-->
     <div class="row">
         <div class="col-md-3">
           <div class="form-group">
             <label>ID</label>
-            <imput type="text" class="form-control" name="id" readonly/> 
+            <input type="text" class="form-control" name="id" readonly/> 
           </div>
         </div>
         <div class="col-md-9">
@@ -49,22 +52,66 @@
         </div>
     </div>
 
-    <!--linha um id, nome-->
+    <!--linha dois data nascimento, endereço-->
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-9"></div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Data de Nascimento</label>
+            <input type="date" class="form-control" name="data-nascimento"/> 
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="form-group">
+            <label>Endereço</label>
+            <input type="text" class="form-control" name="endereco"/>
+          </div>
+        </div>
     </div>
 
-    <!--linha um id, nome-->
+    <!--linha tres cpf, telefone-->
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-9"></div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>CPF</label>
+            <input type="text" class="form-control" name="cpf"/> 
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="form-group">
+            <label>Telefone</label>
+            <input type="number" class="form-control" name="telefone"/>
+          </div>
+        </div>
     </div>
 
-    <!--linha um id, nome-->
+    <!--linha quatro cidade, e-mail-->
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-9"></div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Cidade</label>
+            <input type="text" class="form-control" name="cidade"/>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>E-mail</label>
+            <input type="text" class="form-control" name="email"/>
+          </div>
+        </div>
+    </div>
+
+    <!--adicionando botoes-->
+    <div class="row">
+      <div class="col-md-2">
+        <button type="submit" class="btn btn-primary">
+          SALVAR <span class="glyphicon glyphicon-plus-sign"></span>
+        </button>
+      </div>  
+      <div class="col-md-2">
+        <button type="reset" class="btn btn-info">
+          LIMPAR <span class="glyphicon glyphicon-erase"></span>
+        </button>
+      </div>
     </div>
     </form>
   
