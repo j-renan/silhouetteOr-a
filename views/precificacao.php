003d8fdc1b196dd59e62f724475fdedf40e4bb5e
Renan <desen.renan@gmail.com>
2018-07-16 20:18:20 -0300

    exemplo de duplicação de campos na pagina de precificação

--- views/precificacao.php
+++ views/precificacao.php
@@ -18,38 +18,132 @@
   <br/>
   <form action="">
   <!--linha um nome, preço unitario-->
-    <div class="row">
-        <div class="col-md-6">
-          <div class="form-group">
-            <h3>Produto a Precificar</h3> 
-              <br/>
-              <select class="form-control">
-                <option>Selecione o Produto</option>
-              </select>
-          </div>
-        </div>    
+
+<div class="row">
+
+  <div class="col-md-6">
+    <div class="form-group">
+      <h3>Produto a Precificar</h3> 
+      <br/>
+      <select class="form-control">
+        <option>Selecione o Produto</option>
+      </select>
     </div>
-<br/>
-    <div class="row">
-      <div class="col-md-6">
-          <div class="form-group">
-            <h3>Materiais a Utilizar</h3>
-              <br/>
-              <label>Material</label>
-            <select class="form-control">
-              <option>Selecione o Material</option>
-            </select>
-        </div>
-      </div>
-      <div class="col-md-3">
-          <div class="form-group">
-              <h3>Materiais a Utilizar</h3>
-              <br/>
-            <label>Preço unitário R$</label>
-            <input type="text" class="form-control" name="id" readonly/>
-          </div>
-        </div>
+  </div>  
+
+</div>
+
+  <br/>
+  
+<div class="row">
+
+  <div class="col-md-6">
+    <div class="form-group">
+      <h3>Materiais a Utilizar</h3>
+      <br/>
+      <label>Material</label>
+      <select class="form-control">
+        <option>Selecione o Material</option>
+      </select>
     </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="form-group">
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <label>Preço unitário R$</label>
+        <input type="text" class="form-control" name="id" readonly/>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>              
+    <button type="button" class="btn btn-info" onclick="duplicarCampos();">
+      <span class="glyphicon glyphicon-plus-sign"></span>
+    </button>
+  </div> 
+
+</div>
+
+
+
+<script>
+    function duplicarCampos(){
+	var clone = document.getElementById('origem').cloneNode(true);
+	var destino = document.getElementById('destino');
+	destino.appendChild (clone);
+	var camposClonados = clone.getElementsByTagName('input');
+	for(i=0; i<camposClonados.length;i++){
+		camposClonados[i].value = '';
+	}
+}
+function removerCampos(id){
+	var node1 = document.getElementById('destino');
+	node1.removeChild(node1.childNodes[0]);
+}
+</script>
+
+
+<script>
+  function mostrar(){
+    document.getElementById('origem').style.visibility = 'hidden';
+    }
+  function ocultar(){
+    var ocultar = document.getElementById('origem').style.visibility = 'visible';
+    for(i=0; i<ocultar.length;i++){
+		ocultar[i].value = '';
+	}
+    }
+</script>
+
+
+<div class="row" id="origem">
+  <div class="col-md-6">
+    <div class="form-group">
+      <h3>Materiais a Utilizar</h3>
+      <br/>
+      <label>Material</label>
+      <select class="form-control">
+        <option>Selecione o Material</option>
+      </select>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="form-group">
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <label>Preço unitário R$</label>
+        <input type="text" class="form-control" name="id" readonly/>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>              
+    <button type="button" class="btn btn-info" onclick="duplicarCampos();">
+      <span class="glyphicon glyphicon-plus-sign"></span>
+    </button>
+  </div> 
+</div>
+
+<div id="destino">
+</div>
+
+
+
 
     
 
