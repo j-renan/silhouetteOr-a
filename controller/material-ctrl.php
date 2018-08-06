acc0db1174ab08707044aa0c6c50d9b58775ab84
Moises <molimpio@hotmail.com.br>
2018-08-06 20:18:04 -0300

    adicionado controller

--- controller/material-ctrl.php
+++ controller/material-ctrl.php
@@ -1,5 +1,15 @@
 <?php
+include '../model/Material.php';
+include '../dao/MaterialDAO.php';
 
+$material = $_POST['material'];
+$preco = $_POST['preco'];
 
+$materialDAO = new MaterialDAO();
+$material = new Material(null, $material, $preco);			
+
+$materialDAO->cadastrar($material);
+
+header("Location: http://localhost/projeto-silhouette-orca/views/material.php");
 
 ?>
