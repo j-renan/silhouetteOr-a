831c771774bb23e2ba5fcfc2586141d245ffe5ae
Renan <desen.renan@gmail.com>
2018-10-15 20:44:11 -0300

    arrumando a pagina precificação

--- views/precificacao.php
+++ views/precificacao.php
@@ -20,134 +20,52 @@
   <!--linha um nome, preço unitario-->
 
 <div class="row">
-
-  <div class="col-md-6">
+<div class="col-md-2">
     <div class="form-group">
-      <h3>Produto a Precificar</h3> 
-      <br/>
+      <label>Cliente</label> 
       <select class="form-control">
-        <option>Selecione o Produto</option>
+        <option>Moises</option>
+        <option>Renan</option>
       </select>
     </div>
   </div>  
 
-</div>
-
-  <br/>
-  
-<div class="row">
-
-  <div class="col-md-6">
+  <div class="col-md-2">
     <div class="form-group">
-      <h3>Materiais a Utilizar</h3>
-      <br/>
-      <label>Material</label>
+      <label>Produto a Precificar</label>
+      <select class="form-control">
+        <option>Latinha</option>
+        <option>Caixa Milk</option>
+      </select>
+    </div>
+  </div>  
+
+  <div class="col-md-2">
+    <div class="form-group">
+      <label>Materiais a Utilizar</label>      
       <select class="form-control">
         <option>Selecione o Material</option>
       </select>
     </div>
   </div>
 
-  <div class="col-md-3">
-    <div class="form-group">
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+<div class="col-md-2">
+    <div class="form-group">      
       <label>Preço unitário R$</label>
-        <input type="text" class="form-control" name="id" readonly/>
+        <input type="text" class="form-control" name="id"/>
     </div>
   </div>
 
-  <div class="col-md-2">
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>              
-    <button type="button" class="btn btn-info" onclick="duplicarCampos();">
-      <span class="glyphicon glyphicon-plus-sign"></span>
-    </button>
-  </div> 
 
-</div>
-
-
-
-<script>
-    function duplicarCampos(){
-	var clone = document.getElementById('origem').cloneNode(true);
-	var destino = document.getElementById('destino');
-	destino.appendChild (clone);
-	var camposClonados = clone.getElementsByTagName('input');
-	for(i=0; i<camposClonados.length;i++){
-		camposClonados[i].value = '';
-	}
-}
-function removerCampos(id){
-	var node1 = document.getElementById('destino');
-	node1.removeChild(node1.childNodes[0]);
-}
-</script>
-
-
-<script>
-  function mostrar(){
-    document.getElementById('origem').style.visibility = 'hidden';
-    }
-  function ocultar(){
-    var ocultar = document.getElementById('origem').style.visibility = 'visible';
-    for(i=0; i<ocultar.length;i++){
-		ocultar[i].value = '';
-	}
-    }
-</script>
-
-
-<div class="row" id="origem" style="display:none">
-  <div class="col-md-6">
-    <div class="form-group">
-      <h3>Materiais a Utilizar</h3>
-      <br/>
-      <label>Material</label>
-      <select class="form-control">
-        <option>Selecione o Material</option>
-      </select>
+<div class="col-md-2">
+    <div class="form-group">      
+      <label>Quantidade</label>
+        <input type="number" class="form-control" name="id"/>
     </div>
+  </div>  
   </div>
-
-  <div class="col-md-3">
-    <div class="form-group">
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <label>Preço unitário R$</label>
-        <input type="text" class="form-control" name="id" readonly/>
-    </div>
-  </div>
-
-  <div class="col-md-2">
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>              
-    <button type="button" class="btn btn-info" onclick="duplicarCampos();">
-      <span class="glyphicon glyphicon-plus-sign"></span>
-    </button>
-  </div> 
-</div>
-
-<div id="destino">
-</div>
-
-
-
-
-    
-
-    <!--adicionando botoes-->
+  <div>
+  <!--adicionando botoes-->
     <div class="row">
       <div class="col-md-2">
         <button type="submit" class="btn btn-primary">
@@ -156,8 +74,13 @@ function removerCampos(id){
       </div>      
     </div>
     </form>
-
   </div>
+
+  
+
+
+
+  
 
     <!--incluindo arquivos necessarios para a biblioteca-->
     <script src="../assets/js/jquery.min.js"></script>
