86fbd178f7fc449ff8300783aafb50b5ae64ba81
Renan <desen.renan@gmail.com>
2018-10-29 22:03:00 -0200

    adicionando itens na tabela

--- views/precificacao.php
+++ views/precificacao.php
@@ -46,7 +46,7 @@ include "../controller/buscarDadosPrecificacao.php";
                         <?php
                         for ($i = 0; $i < count($listaProdutos); $i++) {
                             $linha = $listaProdutos[$i];
-                            echo '<option value="' . $linha["preco"] . '">' . $linha["produto"] . '</option>';
+                            echo '<option value="' . $linha["produto"] . '">' . $linha["produto"] . '</option>';
                         }
                         ?>
                     </select>
@@ -60,7 +60,8 @@ include "../controller/buscarDadosPrecificacao.php";
                         <?php
                         for ($i = 0; $i < count($listaMateriais); $i++) {
                             $linha = $listaMateriais[$i];
-                            echo '<option value="' . $linha["material"] . '">' . $linha["material"] . '</option>';
+                            $materialPreco = $linha["material"]."|".$linha["preco"];
+                            echo '<option value="' . $materialPreco . '">' . $linha["material"] . '</option>';
                         }
                         ?>
 
@@ -70,7 +71,14 @@ include "../controller/buscarDadosPrecificacao.php";
 
             <div class="col-md-2">
                 <div class="form-group">
-                    <label>Preço unitário R$</label>
+                    <label>Preço custo R$</label>
+                    <input type="text" class="form-control" name="preco" id="preco" />
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label>Preço venda R$</label>
                     <input type="text" class="form-control" name="preco" id="preco" />
                 </div>
             </div>
@@ -97,19 +105,22 @@ include "../controller/buscarDadosPrecificacao.php";
     <br>
     <div class="row">
         <div class="col-md-12">
-            <table class="table table-condensed table-bordered table-hover table-striped">
+            <table id="tabela" class="table table-condensed table-bordered table-hover table-striped">
                 <thead>
                 <tr>
                     <td>CLIENTE</td>
                     <td>PRODUTO</td>
                     <td>MATERIAL</td>
-                    <td>PREÇO</td>
+                    <td>PREÇO CUSTO</td>
+                    <td>PREÇO VENDA</td>
                     <td>QUANTIDADE</td>
-                    <td>TOTAL</td>
+                    <td>TOTAL CUSTO</td>
+                    <td>TOTAL VENDA</td>
+                    <td>LUCRO</td>
                     <td>EXCLUIR</td>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody id="corpo">
 
                 </tbody>
             </table>
@@ -124,19 +135,57 @@ include "../controller/buscarDadosPrecificacao.php";
     <script>
         const listaMateriais = <?php echo $listaMateriaisJS ?>;
         const precoInput = document.getElementById("preco");        
-        precoInput.value = listaMateriais[0].preco   
+        precoInput.value = listaMateriais[0].preco
 
         function setarPreco() {
             const selectMaterial = document.getElementById("selectMaterial");
-            precoInput.value = selectMaterial.value
+            const dados = selectMaterial.value.split("|")
+            precoInput.value = dados[1]
         }
+
         function adicionarDados() {
             const selectCliente = document.getElementById("selectCliente").value;
             const selectProduto = document.getElementById("selectProduto").value;
             const selectMaterial = document.getElementById("selectMaterial").value;
             const preco = document.getElementById("preco").value;
             const qtde = document.getElementById("qtde").value;
-            console.log(selectCliente, selectProduto, selectMaterial, preco, qtde)
+
+            const nomeMaterial = selectMaterial.split("|")[0]
+
+            criarLinha(selectCliente, selectProduto, nomeMaterial, preco, qtde)
+        }
+
+        function criarLinha(cliente, produto, material, preco, quantidade) {
+
+            const corpo = document.getElementById("corpo")
+            const linha = document.createElement("tr")
+
+            const colCliente = document.createElement("td")
+            const colProduto = document.createElement("td")
+            const colMaterial = document.createElement("td")
+            const colPreco = document.createElement("td")
+            const colQuantidade = document.createElement("td")
+
+            const cli = document.createTextNode(cliente)
+            const pro = document.createTextNode(produto)
+            const mat = document.createTextNode(material)
+            const pre = document.createTextNode(preco)
+            const qua = document.createTextNode(quantidade)
+
+            colCliente.appendChild(cli)
+            colProduto.appendChild(pro)
+            colMaterial.appendChild(mat)
+            colPreco.appendChild(pre)
+            colQuantidade.appendChild(qua)
+
+            linha.appendChild(colCliente)
+            linha.appendChild(colProduto)
+            linha.appendChild(colMaterial)
+            linha.appendChild(colPreco)
+            linha.appendChild(colQuantidade)
+
+
+            corpo.appendChild(linha)
         }
     </script>
 </body>
